02d3d1c7aa84034de327dc1ee9e604a4bdbcc02e
Bao Trinh <github@baodtrinh.com>
2018-11-26 17:39:49 -0600

    fixes

--- zsh/zprofile.zsh
+++ zsh/zprofile.zsh
@@ -1,4 +1,4 @@
 # not needed because we use .plist hack
 # https://evilmartians.com/chronicles/stick-with-security-yubikey-ssh-gnupg-macos
 # eval "$(gpg-agent --daemon --enable-ssh-support)"
-
+[[ -e ~/.profile ]] && emulate sh -c '. ~/.profile'
