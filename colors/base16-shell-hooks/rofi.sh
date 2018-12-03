9a44c61a359fe4dcf3b39620f451b955343c1c44
Bao Trinh <github@baodtrinh.com>
2018-12-03 04:14:12 -0600

    [from now] 2018/12/03 04:14:12
    diff --git a/colors/base16-shell-hooks/rofi.sh b/colors/base16-shell-hooks/rofi.sh
    index 23b6f47..4463b8d 100755
    --- a/colors/base16-shell-hooks/rofi.sh
    +++ b/colors/base16-shell-hooks/rofi.sh
    @@ -5,7 +5,7 @@ for i in $required_vars; do eval "val=\$$i"; if [ -z "$val" ]; then echo "$i is
    
     . "${SCRIPTS_DIR}/sh-update-link.sh" || exit -1
    
    -link_to_update=$CONFIG_DIR/rofi/config
    +link_to_update=$CONFIG_DIR/rofi/base16.conf
     source_file_path=$COLORS_DIR/base16-rofi/themes/base16-$BASE16_THEME.config
    
     update_link "$link_to_update" "$source_file_path"
    diff --git a/compton.conf b/compton.conf
    new file mode 100644
    index 0000000..b386ec5
    --- /dev/null
    +++ b/compton.conf
    @@ -0,0 +1,3 @@
    +opacity-rule = [
    +  "85:class_g = 'URxvt' && !focused"
    +];
    diff --git a/i3/config b/i3/config
    index e6ac1e1..ee09ed8 100644
    --- a/i3/config
    +++ b/i3/config
    @@ -47,7 +47,7 @@
         # set_from_resource $base0F i3wm.color14 #A16946
    
         # set wallpaper
    -    # exec --no-startup-id "feh --bg-fill --randomize --recursive --no-fehbg ~/wallpapers/*"
    +    exec --no-startup-id "feh --bg-fill --randomize --recursive --no-fehbg ~/wallpapers/*"
    
     ### Application behavior
     #
    @@ -431,4 +431,5 @@ bindsym $super+r mode "resize"
         exec --no-startup-id "pidof -x $(basename udiskie) || udiskie"
         exec --no-startup-id "pidof -x $(basename mako) || mako"
         exec --no-startup-id "pidof -x $(basename urxvtd) || urxvtd -q -o -f"
    +    exec --no-startup-id "compton -b -c"
         # exec --no-startup-id "pidof -x $(basename lxqt-policykit-agent) || lxqt-policykit-agent"
    diff --git a/install.conf.yaml b/install.conf.yaml
    index 9e5dd61..a841993 100644
    --- a/install.conf.yaml
    +++ b/install.conf.yaml
    @@ -107,7 +107,7 @@
         # directories
         # -----------
         ~/.config/sway: sway
    -    ~/.config/rofi/config: rofi/config
    +    ~/.config/rofi: rofi
         ~/.Xresources.d: Xresources.d
         ~/.config/i3status: i3status
         ~/.config/i3status-rust: i3status-rust
    @@ -120,6 +120,7 @@
         ~/.Xresources: Xresources
         ~/.xinitrc: xinitrc
         ~/.xprofile: xprofile
    +    ~/.config/compton.conf: compton.conf
    
     # =======
     # Scripts
    diff --git a/rofi/base16.conf b/rofi/base16.conf
    new file mode 120000
    index 0000000..bb4da38
    --- /dev/null
    +++ b/rofi/base16.conf
    @@ -0,0 +1 @@
    +../colors/base16-rofi/themes/base16-classic-dark.config
    \ No newline at end of file
    diff --git a/rofi/config b/rofi/config
    deleted file mode 120000
    index bb4da38..0000000
    --- a/rofi/config
    +++ /dev/null
    @@ -1 +0,0 @@
    -../colors/base16-rofi/themes/base16-classic-dark.config
    \ No newline at end of file
    diff --git a/rofi/config b/rofi/config
    new file mode 100755
    index 0000000..3efce6f
    --- /dev/null
    +++ b/rofi/config
    @@ -0,0 +1,2 @@
    +#include ./base16.conf
    +

--- colors/base16-shell-hooks/rofi.sh
+++ colors/base16-shell-hooks/rofi.sh
@@ -5,7 +5,7 @@ for i in $required_vars; do eval "val=\$$i"; if [ -z "$val" ]; then echo "$i is 
 
 . "${SCRIPTS_DIR}/sh-update-link.sh" || exit -1
 
-link_to_update=$CONFIG_DIR/rofi/config
+link_to_update=$CONFIG_DIR/rofi/base16.conf
 source_file_path=$COLORS_DIR/base16-rofi/themes/base16-$BASE16_THEME.config
 
 update_link "$link_to_update" "$source_file_path"
