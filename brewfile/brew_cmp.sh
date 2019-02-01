6d359cbe5a140bee4b45bf8d99da40f1fb389c09
Bao Trinh <github@baodtrinh.com>
2019-02-01 08:44:11 -0600

    fix dotbot config

--- brewfile/brew_cmp.sh
+++ brewfile/brew_cmp.sh
@@ -2,5 +2,5 @@
 
 brew leaves | # or alternative, brew list -1 |
 xargs sh -c 'for x; do
-  grep ~/.dotfiles/brewfile/Brewfile* -e "$x" > /dev/null || echo $x
+  grep ~/.config/brewfile/Brewfile* -e "$x" > /dev/null || echo $x
 done' # _ | tee brewmissing.txt
